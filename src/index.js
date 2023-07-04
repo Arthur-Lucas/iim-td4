cef5b260e1340ce8324c75975a53655bca2e860c
Arthur <arthurlucas92@orange.fr>
2023-07-04 12:27:19 +0200

    new try

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ function getRandNumber() {
   return Math.floor(Math.random() * 100);
 }
 
-function getRandNumber2() {
-  return Math.floor(Math.random() * 100);
-}
-
 module.exports = getRandNumber;
