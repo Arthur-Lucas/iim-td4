5d13fdc4643a1fbdb548f20247a375560f1d4485
Arthur <arthurlucas92@orange.fr>
2023-07-04 12:26:00 +0200

    retry publish

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ function getRandNumber() {
   return Math.floor(Math.random() * 100);
 }
 
+function getRandNumber2() {
+  return Math.floor(Math.random() * 100);
+}
+
 module.exports = getRandNumber;
